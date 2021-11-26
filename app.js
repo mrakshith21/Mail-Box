d7c932f083050676c4bada5a8cb4bf89ffe99dc7
rakshith <rakshith21mohan@gmail.com>
2021-11-26 20:20:40 +0530

    Sign In, Sign Out, and get profile

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
-
 const app = express();
 
-app.listen("8080", function(){
-    console.log("starting Gmail api");
+app.get("/mail-box", function(req, res){
+    res.render("main.ejs");
+});
+
+app.listen("8887", function(){
 })
