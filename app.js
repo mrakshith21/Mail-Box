fa85e3eef6801d24fd5feba4d43e0816e17da8e7
rakshith <rakshith21mohan@gmail.com>
2021-11-26 22:40:55 +0530

    fetch emails and display

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
 app.get("/mail-box", function(req, res){
     res.render("main.ejs");
 });
