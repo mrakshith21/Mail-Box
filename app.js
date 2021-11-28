3169f13341e09606350be2ddb5fb841f2fd9b0c8
rakshith <rakshith21mohan@gmail.com>
2021-11-28 13:14:58 +0530

    display emails

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.get("/mail-box", function(req, res){
-    res.render("main.ejs");
+    
+    res.render("main");
 });
 
 app.listen("8887", function(){
