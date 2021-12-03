2688c18bd8ef1973a883dfff64c254936a0600e4
Rakshith Mohan <76608847+mrakshith21@users.noreply.github.com>
2021-12-03 23:14:03 +0530

    Create keys.js
    Added keys.js

---
+++ javascript/keys.js
@@ -0,0 +1,7 @@
+function getKeys(){
+    let keys = {
+        "CLIENT_ID" : "Your client ID",
+        "API_KEY" : "Your API Key"                                                
+    };
+    return keys;
+}
