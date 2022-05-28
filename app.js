e5c013d089928c5618f4011612ef812773459a3b
Rakshith <rakshith21mohan@gmail.com>
2022-05-28 15:37:54 +0530

    some changes

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const app = express();
-const dotenv = require('dotenv').config();
 app.set('view engine', 'ejs');
-app.locals.API_KEY = process.env.API_KEY;
-app.locals.CLIENT_ID = process.env.CLIENT_ID;
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/javascript'));
 app.get("/mail-box", function(req, res){
